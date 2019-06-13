079b82a4ca8cee3d2fa337d63e484f4aec476c8c
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-13 23:25:20 +0300

    Updated logo (header) component to functional

--- src/app/components/header/logo/Logo.js
+++ src/app/components/header/logo/Logo.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './../../../images/logo/logo.png';
 
-export default class Logo extends React.Component {
-    render() {
-        return (
-            <div className="logo">
-                <img src={logo} alt="Logotype"/>
-            </div>
-        );
-    }
+export default function(props) {
+    return (
+        <div className="logo">
+            <img src={ logo } alt="Logotype"/>
+        </div>
+    );
 }
