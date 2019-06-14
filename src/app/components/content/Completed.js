18626de79ddb851e84546830ac290888a70870d5
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-14 23:01:49 +0300

    Updated these components to functional

--- src/app/components/content/Completed.js
+++ src/app/components/content/Completed.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import arrows from './../../images/completed/arrows.png';
 
-export default class Completed extends React.Component {
-    render() {
-        return (
-            <section className="completed">
-                <div className="completed_text">
-                    <p className="text">
-                        <span className="text_bold">60%</span>
-                        <br/>
-                        Of project Completed
-                    </p>
-                </div>
-                <div className="completed_arrows">
-                    <img src={arrows} alt="Arrows img"/>
-                </div>
-            </section>
-        );
-    }
+
+export default function(props) {
+    return (
+        <section className="completed">
+            <div className="completed_text">
+                <p className="text">
+                    <span className="text_bold">60%</span>
+                    <br/>
+                    Of project Completed
+                </p>
+            </div>
+            <div className="completed_arrows">
+                <img src={arrows} alt="Arrows img"/>
+            </div>
+        </section>
+    );
 }
--- src/app/components/content/Content.js
+++ src/app/components/content/Content.js
@@ -4,15 +4,14 @@ import Completed from './Completed';
 import TabsTop from '../tabs/top/TabsTop';
 import TabsDown from '../tabs/down/TabsDown';
 
-export default class Content extends React.Component {
-    render() {
-        return (
-            <div className="content">
-                <TabsTop/>
-                <About/>
-                <TabsDown/>
-                <Completed/>
-            </div>
-        );
-    }
+
+export default function(props) {
+    return (
+        <div className="content">
+            <TabsTop/>
+            <About/>
+            <TabsDown/>
+            <Completed/>
+        </div>
+    );
 }
