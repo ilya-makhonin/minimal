2120e1bce826334a7a7b244ab682f1eb3f80ea5c
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-14 23:02:07 +0300

    Updated the Component

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -4,14 +4,13 @@ import Header from './../components/header/Header';
 import Content from './../components/content/Content';
 import Footer from './../components/footer/Footer';
 
-export default class Home extends React.Component {
-    render() {
-        return (
-            <div className="wrapper">
-                <Header/>
-                <Content/>
-                <Footer/>
-            </div>
-        );
-    }
+
+export default function(props) {
+    return (
+        <div className="wrapper">
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
 }
