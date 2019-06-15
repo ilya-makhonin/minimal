a9ec3c219c635ebf2c0575454c744c49c51a458b
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-15 23:28:42 +0300

    Updated this component

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -4,12 +4,22 @@ import Header from './../components/header/Header';
 import Content from './../components/content/Content';
 import Footer from './../components/footer/Footer';
 
+import About from '../components/content/About';
+import Completed from '..components/content/Completed';
+import TabsTop from '../components/tabs/top/TabsTop';
+import TabsDown from '../components/tabs/down/TabsDown';
+
 
 export default function(props) {
     return (
         <div className="wrapper">
             <Header/>
-            <Content/>
+            <Content>
+                <TabsTop/>
+                <About/>
+                <TabsDown/>
+                <Completed/>
+            </Content>
             <Footer/>
         </div>
     );
