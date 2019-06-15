58006b8a9c74b1af2007e75bb4c21ead2686aa5f
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-15 23:28:24 +0300

    Deleted extra code

--- src/app/components/content/Content.js
+++ src/app/components/content/Content.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import About from './About';
-import Completed from './Completed';
-import TabsTop from '../tabs/top/TabsTop';
-import TabsDown from '../tabs/down/TabsDown';
 
 
 export default function(props) {
     return (
         <div className="content">
-            <TabsTop/>
-            <About/>
-            <TabsDown/>
-            <Completed/>
+            { props.children }
         </div>
     );
 }
